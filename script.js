74d4532917442c8299aedf6b8a08d959792f7ea9
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2018-04-07 16:45:22 +0900

    fix non-semicolon lines

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 class Random{
     constructor(seed=20182133){
-        this.y=seed
+        this.y=seed;
     }
     rand(){
         this.y=this.y^(this.y<<13);
         this.y=this.y^(this.y>>17);
         this.y=this.y^(this.y<<15);
-        return this.y
+        return this.y;
     }
     randrange(begin,end){ //range的なのでbegin<=res<end
         return Math.abs(this.rand())%(end-begin)+begin;
